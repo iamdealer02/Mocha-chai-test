1396edd846ae0f545d69107ece00ee50b944d2dc
Upasana Sharma <sharmaupasana823@gmail.com>
2024-05-27 16:04:05 +0200

    app.js creation - for cleanup the server.js

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-const config =  require('./config.js');
-const express = require('express');
-const mongoose = require('mongoose');
-const noteRoutes = require('./src/routes/note.routes');
+const app = require('./src/app');
+const { PORT,NODE_ENV } = require('./config');
 
-const { PORT,NODE_ENV, MONGO_URI} = config;
-
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const server = app.listen(PORT, () => {
+	console.log(`Application running on port: ${PORT} in ${NODE_ENV} mode`);
 });
-const app = express();
 
-app.use(express.json());
-app.use('/api', noteRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} IN THE MODE ${NODE_ENV} .`);
-});
+module.exports = server;
