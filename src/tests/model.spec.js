6b12642a2e4727d200fd348c298311c47bd781b7
Upasana Sharma <sharmaupasana823@gmail.com>
2024-05-27 17:12:21 +0200

    Unit Test : -Model testing for note routes

---
+++ src/tests/model.spec.js
@@ -0,0 +1,45 @@
+const chai = require('chai');
+const expect = chai.expect;
+const mongoose = require('mongoose');
+const ValidationError = mongoose.Error.ValidationError;
+
+
+var Note = require('../models/note.model');
+
+describe('Note Model Test', () => {
+    let sampleNoteValue;
+    beforeEach(() => {
+        sampleNoteValue = {
+            content: "Test Content"
+        };
+    });
+    
+    // missing fields
+    it('it should throw an error due to missing fields', async () => {
+        const note = new Note();
+        note.validate((err) => {
+            expect(err.errors.title).to.exist;
+            expect(err.errors.content).to.exist;
+            done();
+        });
+    });
+    it('should create a new note with correct params', async () => {
+        const note = new Note({
+            ...sampleNoteValue,
+            title: 'Test Title'
+        });
+        note.validate((err) => {
+            if(err){
+                const unexpectedError = new Error('⚠️ Unexpected error!');
+                done(unexpectedError);
+            }else{
+                expect(note).to.be.an('object');
+                expect(note).to.have.property('title', 'Test Title');
+                expect(note.title).to.equal('Test Title');
+                done();
+            }
+    });
+    
+    });
+
+});
